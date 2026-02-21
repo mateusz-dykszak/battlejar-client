1c70d269d95c2b18a8462f9f99981f3b2d41fa3d
Mateusz Dykszak <mateusz@dykszak.pl>
2026-02-21 14:15:10 +0100

    Add `random` method to `Color` enum for selecting random colors

--- api/src/main/java/it/battlejar/api/Color.java
+++ api/src/main/java/it/battlejar/api/Color.java
@@ -1,5 +1,7 @@
 package it.battlejar.api;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Represents the possible colours for players and entities in the game.
  */
@@ -13,6 +15,16 @@ public enum Color {
      */
     public static int count() {
         return values().length - 1;
+    }
+
+    /**
+     * Selects a random color from the available color options, excluding {@code NONE}.
+     *
+     * @return A randomly chosen {@code Color} from the enum, excluding {@code NONE}.
+     */
+    public static Color random() {
+        Color[] colors = Color.values();
+        return colors[ThreadLocalRandom.current().nextInt(count())];
     }
 }
 
